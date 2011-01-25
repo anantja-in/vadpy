2fdf378668053e5f63dcc9b5e88e6dca52af5a36
Zaur Nasibov <znasibov@cs.joensuu.fi>
2011-01-25 08:41:46 +0200

    Tabs in iosingled output

--- modules/io_singled.py
+++ modules/io_singled.py
@@ -70,5 +70,5 @@ class IOSingleD(IOModule):
         super(IOSingleD, self).write(data, path)
         with open(path, 'w') as f:
             for section in data:
-                f.write('{0} {1} {2}\n'.format(section[0], section[1], int(section[2])) )                
+                f.write('{0}\t{1}\t{2}\n'.format(section[0], section[1], int(section[2])) )                
     
